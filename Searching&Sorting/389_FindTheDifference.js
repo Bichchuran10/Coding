b0e61c85544589ebb9c18ef6f3ad0b91d6c9cc67
Bichchuran10 <bichchuran10@gmail.com>
2023-03-13 22:23:42 +0530

    different character

---
+++ Searching&Sorting/389_FindTheDifference.js
@@ -0,0 +1,34 @@
+// Example 1:
+
+// Input: s = "abcd", t = "abcde"
+// Output: "e"
+// Explanation: 'e' is the letter that was added.
+// Example 2:
+
+// Input: s = "", t = "y"
+// Output: "y"
+
+// Constraints:
+
+// 0 <= s.length <= 1000
+// t.length == s.length + 1
+// s and t consist of lowercase English letters.
+
+
+function findDifference(s,t)
+{
+    let arr1=s.split("").sort()
+    let arr2=t.split("").sort()
+    console.log(arr1)
+    console.log(arr2)
+
+    for(let i=0;i<arr2.length;i++)
+    {
+        if(arr1[i]!=arr2[i])
+        {
+            return arr2[i]
+        }
+    }
+}
+
+console.log(findDifference("abcd","abcde"))
